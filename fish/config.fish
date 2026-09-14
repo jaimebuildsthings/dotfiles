1181fe40bfbc7e3c5d018ad0fadb660d1cc2ce8a
Jaime Liz <lizjaime@gmail.com>
2026-09-14 09:25:46 -0500

    feat: add initial nvim config and update Brewfile

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,6 @@ source ~/.config/fish/aliases.fish
 # Load cargo
 source "$HOME/.cargo/env.fish"
 
-# Load starship
-starship init fish | source
-
 # ASDF configuration code
 if test -z $ASDF_DATA_DIR
     set _asdf_shims "$HOME/.asdf/shims"
